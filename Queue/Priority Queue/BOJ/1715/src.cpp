a223a474e855978a3f5974cccba6c7454d964922
seonpilKim <kimsunpil123@naver.com>
2021-07-18 01:18:05 +0900

    [1715] 카드 정렬하기

---
+++ Queue/Priority Queue/BOJ/1715/src.cpp
@@ -0,0 +1,50 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <math.h>
+#include <bitset>
+#include <stack>
+#include <queue>
+#include <math.h>
+#include <deque>
+#include <map>
+#include <set>
+#include <unordered_set>
+#include <unordered_map>
+#include <limits.h>
+#include <numeric>
+#include <functional>
+#include <sstream>
+#include <bitset>
+
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+
+	int N;
+	cin >> N;
+
+	priority_queue<int, vector<int>, greater<int>> card;
+	for (int i = 0; i < N; i++) {
+		int n;
+		cin >> n;
+		card.push(n);
+	}
+
+	int ans = 0;
+	while (card.size() > 1) {
+		int sum = card.top();
+		card.pop();
+		sum += card.top();
+		card.pop();
+		card.push(sum);
+		ans += sum;
+	}
+	
+	cout << ans;
+
+	return 0;
+}
